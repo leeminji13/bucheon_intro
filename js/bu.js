8879e86baad3f5023493316dea1dead25ef73c40
leeminji <minjis2s2s2@gmail.com>
2022-07-01 18:47:59 +0900

    텍스트,이미지 슬라이드 구현/네비게이션 토글 구현 오류 잡아야함

--- js/bu.js
+++ js/bu.js
@@ -7,5 +7,36 @@
        faul.classList.toggle('d-none');
     });
 
+//메인스와이퍼
+    let current = 0;
+    //li 위치를 나타내는 변수
+    showSlides();
+    //기본 첫 번째 li를 보여줌
+
+    function showSlides(){
+        let slides = document.querySelectorAll(".textSwiper > li");
+        for (let i = 0; i < slides.length; i++){
+            slides[i].style.opacity = "0";
+            //모든 li 감춤
+        }
+        current++;
+        //다음 li로 이동
+        if(current > slides.length) //마지막 li라면
+            current = 1;//첫번째로 이동
+            slides[current - 1].style.opacity = "1";//현재 위치 li표시
+            setTimeout(showSlides, 800)//0.8초마다 showSlides()함수를 반복 실행
+
+    }
+
+//네비게이션
+    let trigger = document.querySelector("#trigger")
+    let inner = document.querySelector(".inner")
+    let menuWrap = document.querySelector(".menuWrap")
+
+    trigger.addEventListener('click', function(){
+        inner.classList.toggle('naviStyle');
+    })
+
+
 
 
