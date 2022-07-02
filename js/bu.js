b6f6f8b498ecfe42b2525bebb789dd5ee3fa2874
leeminji <minjis2s2s2@gmail.com>
2022-07-02 14:44:35 +0900

    좌우슬라이드 삽입,무한루프/자동슬라이드 추가해야함

--- js/bu.js
+++ js/bu.js
@@ -2,29 +2,24 @@
     let family = document.querySelector('.family')
     let faul = document.querySelector('.faul')
 
-    family.addEventListener('click',function(){
-        //클릭하면 실제로 할 일
+    family.addEventListener('click',function(){ //클릭하면 실제로 할 일
        faul.classList.toggle('d-none');
     });
 
-//메인스와이퍼
-    let current = 0;
-    //li 위치를 나타내는 변수
-    showSlides();
-    //기본 첫 번째 li를 보여줌
+//메인슬라이드
+    let current = 0; //li 위치를 나타내는 변수
+    showSlides(); //기본 첫 번째 li를 보여줌
 
     function showSlides(){
         let slides = document.querySelectorAll(".textSwiper > li");
         for (let i = 0; i < slides.length; i++){
-            slides[i].style.opacity = "0";
-            //모든 li 감춤
+            slides[i].style.opacity = "0"; //모든 li 감춤
         }
-        current++;
-        //다음 li로 이동
+        current++; //다음 li로 이동
         if(current > slides.length) //마지막 li라면
             current = 1;//첫번째로 이동
             slides[current - 1].style.opacity = "1";//현재 위치 li표시
-            setTimeout(showSlides, 800)//0.8초마다 showSlides()함수를 반복 실행
+            setTimeout(showSlides, 800) //0.8초마다 showSlides()함수를 반복 실행
 
     }
 
@@ -36,6 +31,69 @@
     trigger.addEventListener('click', function(){
         inner.classList.toggle('naviStyle');
     })
+
+//이미지 슬라이드
+    let img = document.querySelector(".imgSlide > img");
+    width = img.width;
+    let forlength = document.querySelectorAll(".imgSlide > img");
+    length = forlength.length
+    let curPos = 0;
+    let postion = 0;
+    let prevBtn = document.querySelector(".prevBtn")
+    let nextBtn = document.querySelector(".nextBtn")
+    let imgSlide = document.querySelector(".imgSlide");
+    
+    // let lele = 0;
+    // let i = 0;
+    // function moveleft() {
+    //     if (i < length - 1) {
+    //         lele += 100;
+    //         i++;
+    //         imgSlide.style.transition = '.3s'
+    //         setTimeout('moveleft()', 3000);
+    //     } else {
+    //         imgSlide.style.transition = '0s'
+    //         lele = 0;
+    //         i = 0;
+    //         setTimeout('moveleft()', 0);
+    //     }
+    //      imgSlide.style.transform = `translateX("-" + ${lele} + "%")`;
+    // }
+    // moveleft();
+
+    function prev(){
+        if(curPos > 0){
+            nextBtn.removeAttribute("disabled")
+            postion += width;
+            imgSlide.style.transform = `translateX(${postion}px)`;
+            imgSlide.style.transition = '0.5s'
+            curPos = curPos - 1;
+        }
+        if(curPos == 0){
+            prevBtn.setAttribute('disabled', 'true')
+        }
+    }
+    function next(){
+        if(curPos < 3){
+            prevBtn.removeAttribute("disabled")
+            postion -= width;
+            imgSlide.style.transform = `translateX(${postion}px)`;
+            imgSlide.style.transition = '0.5s'
+            curPos = curPos + 1;
+        }
+        if(curPos == 3){
+            nextBtn.setAttribute('disabled', 'true')
+        }
+    }
+    
+    function init(){
+    prevBtn.setAttribute('disabled', 'true')
+    prevBtn.addEventListener("click", prev)
+    nextBtn.addEventListener("click", next)
+    nextBtn.setAttribute('disabled', 'true')
+    }
+    
+    init();
 
 
 
